78d149db82343168e69403eeef2572b71d40d1e6
Pc Daniel <danielfernandes202@gmail.com>
2025-08-15 12:30:59 -0300

    teste fix entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python src/manage.py migrate --noinput
-python src/manage.py collectstatic --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
 gunicorn app.wsgi:application --bind 0.0.0.0:8000
