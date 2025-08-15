ae722c7e75c59a4616e53171201a0df7d162494e
Pc Daniel <danielfernandes202@gmail.com>
2025-08-15 12:54:56 -0300

    aumentando worker e threads do gunicorn

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn app.wsgi:application --bind 0.0.0.0:8000
+gunicorn app.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 2
+
