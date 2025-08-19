17bc017800ba4f6191c86a8253a7a2a98f74e4da
Pc Daniel <danielfernandes202@gmail.com>
2025-08-19 11:09:56 -0300

    limitar workers do backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn app.wsgi:application --bind 0.0.0.0:8000 --workers 4 --threads 2
+gunicorn app.wsgi:application --bind 0.0.0.0:8000 --workers 1 --threads 2
 
