87bd20c3dfff52900d56f57a466857aae5e59129
Pc Daniel <danielfernandes202@gmail.com>
2025-09-10 12:11:16 -0300

    fix bug establishmentName

--- frontend/src/shared/schemas/establishment.ts
+++ frontend/src/shared/schemas/establishment.ts
@@ -3,7 +3,7 @@ import City from "./city";
 export default interface Establishment {
     id: number;
     name: string;
-    acronym: string;
+    acronym?: string;
     cnes: string;
     city: City;
     is_active: boolean;
