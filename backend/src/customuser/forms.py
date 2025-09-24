2cb9b284bc4e23d6c37726a6082fc9d1b9aa5a1b
Pc Daniel <danielfernandes202@gmail.com>
2025-09-24 16:44:37 -0300

    remove password_validation

--- backend/src/customuser/forms.py
+++ backend/src/customuser/forms.py
@@ -1,10 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django import forms
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
         fields = ('username', 'email', 'city', 'role')
+
+    def _post_clean(self):
+        # Não chama password_validation (ignora validadores de senha)
+        super(forms.ModelForm, self)._post_clean()
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
