8b93b4798325c5deea0cb46c500436de3e2a8496
Pc Daniel <danielfernandes202@gmail.com>
2025-09-30 15:47:32 -0300

    permitir que o cid tenha procedimento ou não na entidade (facilitar serialização )

--- backend/core/src/apac_core/domain/entities/cid.py
+++ backend/core/src/apac_core/domain/entities/cid.py
@@ -6,5 +6,5 @@ from apac_core.domain.entities.procedure import Procedure
 class Cid(BaseModel):
     code: str
     name: str
-    procedure: Procedure
+    procedure: Optional[Procedure] = None
     id: Optional[int] = None  # ID opcional até ser salvo
