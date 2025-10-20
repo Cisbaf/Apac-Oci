601a4536ce4d16bcdcaeb815ab50e54a2fae6ff2
Pc Daniel <danielfernandes202@gmail.com>
2025-10-20 14:04:26 -0300

    fix prefixo bom file download

--- frontend/src/app/extracao/controllers/Downloads.ts
+++ frontend/src/app/extracao/controllers/Downloads.ts
@@ -1,18 +1,17 @@
 
 export function GenerateStringToDownloadFile(filename: string, content: string) {
-        const blob = new Blob([content], { type: "text/plain" });
+    // Prefixo BOM
+    const bom = '\uFEFF';
+    const blob = new Blob([bom + content], { type: "text/plain;charset=utf-8" });
 
-        // gera uma URL temporária
-        const url = window.URL.createObjectURL(blob);
+    const url = window.URL.createObjectURL(blob);
 
-        // cria um link "invisível"
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = filename; // nome do arquivo
-        document.body.appendChild(a);
-        a.click();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
 
-        // limpa recursos
-        a.remove();
-        window.URL.revokeObjectURL(url);
+    a.remove();
+    window.URL.revokeObjectURL(url);
 }
