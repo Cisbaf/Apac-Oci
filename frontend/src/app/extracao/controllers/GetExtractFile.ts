31fa1eed1898fdd590d543e1b4492c2c2a4192ea
Pc Daniel <danielfernandes202@gmail.com>
2025-11-07 13:35:22 -0300

    forçando competência ser o primeiro dia

--- frontend/src/app/extracao/controllers/GetExtractFile.ts
+++ frontend/src/app/extracao/controllers/GetExtractFile.ts
@@ -2,11 +2,14 @@ import { ExtractFormData } from "../schemas/extractForm";
 import { formatDateToYMD } from "@/app/solicitar/apacRequest/utils/dateUtils";
 
 export default async function GetExtractFile(data: ExtractFormData) {
+    data.production.setDate(1)
     const newData = {
         production: formatDateToYMD(data.production),
         establishment_id: data.establishmentId,
         apac_batchs: data.apacBatchs
     };
+
+    console.log(newData)
 
     const response = await fetch("/api/proxy/apac_batch/extract", {
         method: "POST",
