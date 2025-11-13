4f76cb9450acccaca570f92674efc2459f5c9533
Pc Daniel <danielfernandes202@gmail.com>
2025-11-13 15:11:44 -0300

    opção para restringir um usuário visualizar municipio

--- backend/src/establishment/models.py
+++ backend/src/establishment/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from city.models import CityModel
 from apac_core.domain.entities.establishment import Establishment
-
+from customuser.models import CustomUser
 
 class EstablishmentModel(models.Model):
-    cnes = models.CharField(verbose_name="Cnes Estabelecimento", max_length=15, unique=True)
     name = models.CharField(verbose_name="Nome do Estabelecimento", max_length=100, unique=True)
+    cnes = models.CharField(verbose_name="Cnes Estabelecimento", max_length=15, unique=True)
     city = models.ForeignKey(
         verbose_name="Cidade",
         to=CityModel,
@@ -14,13 +14,14 @@ class EstablishmentModel(models.Model):
     )
     cnpj = models.CharField(verbose_name="CNPJ", max_length=14)
     acronym = models.CharField(verbose_name="Sigla", max_length=255)
+    restricted_user = models.ManyToManyField(to=CustomUser, verbose_name="Restringir visualização", null=True, blank=True)
     is_active = models.BooleanField(verbose_name="Está Ativo", default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'estabelecimentos'
-        verbose_name = "Estabelecimentp"
+        verbose_name = "Estabelecimento"
         verbose_name_plural = "Estabelecimentos"
 
     def to_entity(self):
